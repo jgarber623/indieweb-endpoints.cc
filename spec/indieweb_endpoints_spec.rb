6cc4545408a90598eba08eccd764d54f5e4305f2
Jason Garber <jason@sixtwothree.org>
2022-04-21 11:03:10 -0400

    Add failing 404 specs

--- spec/indieweb_endpoints_spec.rb
+++ spec/indieweb_endpoints_spec.rb
@@ -9,4 +9,25 @@ RSpec.describe IndieWebEndpoints do
     it { expect(response).to be_ok }
     it { expect(response.body).to include('Enter a URL to get started') }
   end
+
+  describe 'GET /foo' do
+    let(:message) { 'The requested URL could not be found' }
+
+    context 'when requesting text/html' do
+      let(:response) { get '/foo' }
+
+      it { expect(response).to be_not_found }
+      it { expect(response.body).to include(message) }
+    end
+
+    context 'when requesting application/json' do
+      let(:response) do
+        header 'Accept', 'application/json'
+        get '/foo'
+      end
+
+      it { expect(response).to be_not_found }
+      it { expect(response.body).to eq({ error: { code: 404, message: message } }.to_json) }
+    end
+  end
 end
