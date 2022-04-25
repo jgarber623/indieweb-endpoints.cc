7901ec5a5e7e8562a3cffe477358d59c90516a71
Jason Garber <jason@sixtwothree.org>
2022-04-25 13:28:37 -0400

    Reorganize specs

--- spec/web/not_found_spec.rb
+++ spec/web/not_found_spec.rb
@@ -3,13 +3,6 @@
 RSpec.describe IndieWebEndpoints do
   subject(:app) { described_class.app }
 
-  describe 'GET /' do
-    let(:response) { get '/' }
-
-    it { expect(response).to be_ok }
-    it { expect(response.body).to include('Enter a URL to get started') }
-  end
-
   describe 'GET /foo' do
     let(:message) { 'The requested URL could not be found' }
 
