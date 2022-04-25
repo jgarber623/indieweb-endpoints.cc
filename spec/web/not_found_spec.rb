edc2eac41e38df1da9f9943aa641c30e18895d96
Jason Garber <jason@sixtwothree.org>
2022-04-25 14:15:39 -0400

    Define app in RSpec config, add shared_context
    Defining `app` in RSpec's config block is a rack-test convention.
    
    Adding the shared context to set a `response` subject will tidy up
    examples in spec/web.

--- spec/web/not_found_spec.rb
+++ spec/web/not_found_spec.rb
@@ -1,25 +1,26 @@
 # frozen_string_literal: true
 
-RSpec.describe IndieWebEndpoints do
-  subject(:app) { described_class.app }
-
+RSpec.describe IndieWebEndpoints, roda: :app do
   describe 'GET /foo' do
     let(:message) { 'The requested URL could not be found' }
 
     context 'when requesting text/html' do
-      let(:response) { get '/foo' }
+      before do
+        header 'Accept', 'text/html'
+        get '/foo'
+      end
 
-      it { expect(response).to be_not_found }
+      it { is_expected.to be_not_found }
       it { expect(response.body).to include(message) }
     end
 
     context 'when requesting application/json' do
-      let(:response) do
+      before do
         header 'Accept', 'application/json'
         get '/foo'
       end
 
-      it { expect(response).to be_not_found }
+      it { is_expected.to be_not_found }
       it { expect(response.body).to eq({ error: { code: 404, message: message } }.to_json) }
     end
   end
