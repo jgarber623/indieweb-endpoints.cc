57ec724e6a330e0abb180e5efd71bb72474f88b6
Jason Garber <jason@sixtwothree.org>
2022-10-09 13:41:53 -0400

    Move public plugin under Rendering comment

--- indieweb_endpoints.rb
+++ indieweb_endpoints.rb
@@ -6,13 +6,13 @@ class IndieWebEndpoints < Roda
   # Routing plugins
   plugin :head
   plugin :not_allowed
-  plugin :public
   plugin :status_handler
   plugin :type_routing, exclude: [:xml]
 
   # Rendering plugins
   plugin :h
   plugin :link_to
+  plugin :public
   plugin :render
 
   # Request/Response plugins
